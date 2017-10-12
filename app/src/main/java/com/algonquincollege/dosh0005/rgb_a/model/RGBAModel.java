1b3da16f282c4683974e5eab94d921774af305c7
Harsh Doshi <dosh0005@algonquinlive.com>
2017-10-12 11:58:35 -0400

    Initial Commit

---
+++ app/src/main/java/com/algonquincollege/dosh0005/rgb_a/model/RGBAModel.java
@@ -0,0 +1,19 @@
+package com.algonquincollege.dosh0005.rgb_a.model;
+
+import java.util.Observable;
+
+/**
+ * The model holds the data.
+ *
+ * Model color.
+ * Based on red, green, blud and alpha (transparency).
+ *
+ * RGB: integer values in the domain range of 0 to 255 (inclusive).
+ * Alpha: integer value in the domain range of 0 to 255 (inclusive).
+ *
+ * @author dosh0005@algonquinlive.com on 2017-10-12.
+ * @version 1.0
+ */
+
+public class RGBAModel extends Observable {
+}
